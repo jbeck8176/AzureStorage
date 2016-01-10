ecc8f675ebdc699e1a2a4412790e08172269e40b
jbeck8176 <jbeck8176@gmail.com>
2016-01-10 01:39:21 -0500

    initial commit, tests and storage tables completed

---
+++ AzureStorage.Tests/TablesTests.cs
@@ -0,0 +1,117 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AzureStorage;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.WindowsAzure.Storage.Table;
+
+namespace AzureStorage.Tests
+{
+    [TestClass()]
+    public class TablesTests
+    {
+        private UnitTestObject testData = new UnitTestObject()
+        {
+            intProperty = 1,
+            stringProperty = "test string",
+            boolProperty = false,
+            datetimeOffsetProperty = DateTimeOffset.Now,
+            dateTimeProperty = DateTime.Now,
+            doubleProperty = 12.34,
+            guidProperty = Guid.NewGuid(),
+            binaryProperty = Encoding.ASCII.GetBytes("test binary")
+        };
+
+        public const string tableName = "UnitTestTable";
+        public string partitionKey = Guid.NewGuid().ToString();
+        public string rowKey = Guid.NewGuid().ToString();
+
+        [TestMethod()]
+        public void InsertOrReplaceTest()
+        {
+
+            try
+            {
+                var tables = new AzureStorage.Tables(Properties.Settings.Default.AzureConnection);
+                tables.InsertOrReplace(tableName, rowKey, partitionKey, testData);
+            }
+            catch (Exception e)
+            {
+                Assert.Fail("Failed to write to storage table: " + e.Message);
+                throw e;
+            }
+
+        }
+
+        [TestMethod()]
+        public void RetrieveTest()
+        {
+            try
+            {
+                InsertOrReplaceTest();
+                var tables = new AzureStorage.Tables(Properties.Settings.Default.AzureConnection);
+
+                var tblResult = tables.Retrieve(tableName, rowKey, partitionKey);
+
+                Assert.AreNotEqual(tblResult.HttpStatusCode, 404);
+
+                var tblEntity = ((Microsoft.WindowsAzure.Storage.Table.DynamicTableEntity)(tblResult.Result));
+
+                Assert.IsNotNull(tblResult);
+
+                CollectionAssert.AreEqual(testData.binaryProperty, tblEntity.Properties["binaryProperty"].BinaryValue);
+                Assert.AreEqual(testData.boolProperty, tblEntity.Properties["boolProperty"].BooleanValue);
+                //Assert.AreEqual(testData.datetimeOffsetProperty, tblEntity.Properties["datetimeOffsetProperty"].DateTimeOffsetValue);
+                //Assert.AreEqual(testData.dateTimeProperty, tblEntity.Properties["dateTimeProperty"].DateTime);
+                Assert.AreEqual(testData.doubleProperty, tblEntity.Properties["doubleProperty"].DoubleValue);
+                Assert.AreEqual(testData.guidProperty, tblEntity.Properties["guidProperty"].GuidValue);
+                Assert.AreEqual(testData.intProperty, tblEntity.Properties["intProperty"].Int32Value);
+                Assert.AreEqual(testData.stringProperty, tblEntity.Properties["stringProperty"].StringValue);
+            }
+            catch (Exception e)
+            {
+                Assert.Fail("Retrieve test failed: " + e.Message);
+            }
+        }
+
+        [TestMethod()]
+        public void RetrieveTestGenericTypes()
+        {
+            try
+            {
+                InsertOrReplaceTest();
+                var tables = new AzureStorage.Tables(Properties.Settings.Default.AzureConnection);
+
+                var retObj = tables.Retrieve<UnitTestObject>(tableName, rowKey, partitionKey);
+
+                CollectionAssert.AreEqual(testData.binaryProperty, retObj.binaryProperty);
+                Assert.AreEqual(testData.boolProperty, retObj.boolProperty);
+                //Assert.AreEqual(testData.datetimeOffsetProperty, tblEntity.Properties["datetimeOffsetProperty"].DateTimeOffsetValue);
+                //Assert.AreEqual(testData.dateTimeProperty, tblEntity.Properties["dateTimeProperty"].DateTime);
+                Assert.AreEqual(testData.doubleProperty, retObj.doubleProperty);
+                Assert.AreEqual(testData.guidProperty, retObj.guidProperty);
+                Assert.AreEqual(testData.intProperty, retObj.intProperty);
+                Assert.AreEqual(testData.stringProperty, retObj.stringProperty);
+            }
+            catch (Exception e)
+            {
+                Assert.Fail("Retrieve test failed: " + e.Message);
+            }
+        }
+    }
+
+    public class UnitTestObject : TableEntity
+    {
+        public int intProperty { get; set; }
+        public string stringProperty { get; set; }
+        public Guid guidProperty { get; set; }
+        public bool boolProperty { get; set; }
+        public DateTime dateTimeProperty { get; set; }
+        public DateTimeOffset datetimeOffsetProperty { get; set; }
+        public double doubleProperty { get; set; }
+        public byte[] binaryProperty { get; set; }
+    }
+
+}
---
+++ AzureStorage/Tables.cs
@@ -0,0 +1,160 @@
+﻿using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Table;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AzureStorage
+{
+    public class Tables
+    {
+        private CloudStorageAccount _account;
+        //Lets keep a dictionary of tables
+        private Dictionary<string, CloudTable> tables = new Dictionary<string, CloudTable>();
+
+        /// <summary>
+        /// Initializes the Tables class using the supplied connection string.
+        /// </summary>
+        /// <param name="connectionString">The connection string from the Azure portal.</param>
+        public Tables(string connectionString)
+        {
+            _account = CloudStorageAccount.Parse(connectionString);
+        }
+
+        /// <summary>
+        /// Gets the CloudTable by name.
+        /// </summary>
+        /// <param name="Name"></param>
+        /// <returns>The cloud table reference</returns>
+        private CloudTable GetTable(string Name)
+        {
+            // If the table reference exists in the dictionary, return it
+            string table = Name.ToString().ToLower().Trim();
+            if (tables.ContainsKey(table))
+            {
+                return tables[table];
+            }
+
+            // Since it doesn't, create a reference to it
+            CloudTableClient tableClient = _account.CreateCloudTableClient();
+            CloudTable currentTable = tableClient.GetTableReference(table);
+
+            // Add to dictionary and return
+            tables.Add(table, currentTable);
+            return currentTable;
+        }
+
+        public void InsertOrReplace(string tableName, string rowKey, string partitionKey, ITableEntity row)
+        {
+            //handle override for when I want to send in my own row key and partition key
+            row.PartitionKey = partitionKey;
+            row.RowKey = rowKey;
+
+            InsertOrReplace(tableName, row);
+        }
+
+        public void InsertOrReplace(string tableName, ITableEntity row)
+        {
+            //get the table we want to save to
+            CloudTable tbl = GetTable(tableName);
+
+            //create the operation for the row
+            TableOperation op = TableOperation.InsertOrReplace(row);
+
+            //execute the operation
+            var res = tbl.Execute(op);
+        }
+
+        public TableResult Retrieve(string table, string rowKey, string partitionKey)
+        {
+            //get the table reference
+            CloudTable tbl = GetTable(table);
+
+            //set up a retrieve table operation
+            var op = TableOperation.Retrieve(partitionKey, rowKey);
+
+            var data = tbl.Execute(op);
+
+
+            return data;
+        }
+
+        public T Retrieve<T>(string table, string rowKey, string partitionKey)
+        {
+            //get the table data from the storage table
+            var tblRes = Retrieve(table, rowKey, partitionKey);
+
+            //get the type return object
+            Type type = typeof(T);
+
+            //get a instance of the return object
+            var retObj = Activator.CreateInstance(type);
+
+            var dteResult = (Microsoft.WindowsAzure.Storage.Table.DynamicTableEntity)(tblRes.Result);
+
+            //set the properties of the TableEntity interface first
+            type.GetProperty("ETag").SetValue(retObj, dteResult.ETag);
+            type.GetProperty("PartitionKey").SetValue(retObj, dteResult.PartitionKey);
+            type.GetProperty("RowKey").SetValue(retObj, dteResult.RowKey);
+            type.GetProperty("Timestamp").SetValue(retObj, dteResult.Timestamp);
+
+            //iterate througn the storage data and handle each property
+            foreach (var prop in dteResult.Properties)
+            {
+                //use reflection to get the info about the return object property
+                var pInfo = type.GetProperty(prop.Key);
+
+                //make sure the property exists in the object
+                if(pInfo != null)
+                {
+                    //switch based on the type of the property and set the object value with reflection
+                    switch (pInfo.PropertyType.Name.ToLower())
+                    {
+                        case "int32":
+                            pInfo.SetValue(retObj, prop.Value.Int32Value);
+                            break;
+                        case "int64":
+                            pInfo.SetValue(retObj, prop.Value.Int64Value);
+                            break;
+                        case "string":
+                            pInfo.SetValue(retObj, prop.Value.StringValue);
+                            break;
+                        case "guid":
+                            pInfo.SetValue(retObj, prop.Value.GuidValue);
+                            break;
+                        case "bool":
+                            pInfo.SetValue(retObj, prop.Value.BooleanValue);
+                            break;
+                        case "datetime":
+                            pInfo.SetValue(retObj, prop.Value.DateTime);
+                            break;
+                        case "datetimeoffset":
+                            pInfo.SetValue(retObj, prop.Value.DateTimeOffsetValue);
+                            break;
+                        case "double":
+                            pInfo.SetValue(retObj, prop.Value.DoubleValue);
+                            break;
+                        case "byte[]":
+                            pInfo.SetValue(retObj, prop.Value.BinaryValue);
+                            break;
+                        case "etag":
+                            pInfo.SetValue(retObj, prop.Value.StringValue);
+                            break;
+                        case "partitionkey":
+                            pInfo.SetValue(retObj, prop.Value.StringValue);
+                            break;
+                        case "rowkey":
+                            pInfo.SetValue(retObj, prop.Value.StringValue);
+                            break;
+
+                    }
+                }
+            }
+
+            //return the return object
+            return (T) retObj;
+        }
+    }
+}
